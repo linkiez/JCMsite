cacb169641dd3c6b1b540f24947ae5604b1c32aa
LiNkIeZ <61906961+linkiez@users.noreply.github.com>
2020-05-27 15:53:08 -0300

    1.2
    -Migração de scripts de tag para arquivos .js

---
+++ website_rev04/gtag.js
@@ -0,0 +1,11 @@
+// Global site tag (gtag.js) - Google Analytics
+
+window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-99109145-1');
+    gtag('config', 'AW-997519795');
