b6c2dc1afc84e9b9ca34bdd892316d305f7e64aa
LiNkIeZ <61906961+linkiez@users.noreply.github.com>
2020-06-24 07:44:07 -0300

    1.6
    - Adição de script de conversão para chamadas telefonicas apartir do site.

--- website_rev04/gtag.js
+++ website_rev04/gtag.js
@@ -9,3 +9,18 @@ window.dataLayer = window.dataLayer || [];
 
     gtag('config', 'UA-99109145-1');
     gtag('config', 'AW-997519795');
+
+    /*<!-- Event snippet for Chamada telefônica a partir site conversion page
+In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->*/
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-997519795/fhpNCIOW89QBELPj09sD',
+      'event_callback': callback
+  });
+  return false;
+}
